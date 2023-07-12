6a9df81177f78ca761fa5693de16dc9ff265a841
Anton <ablichenkov@gmail.com>
2023-07-12 13:13:48 +0300

    EmployeesController and small Model changes

---
+++ VetClinic.Web/Services/TempPasswordService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using VetClinic.Common.Entities;
+
+namespace VetClinic.Web.Services
+{
+    public static class TempPasswordService
+    {
+        public static async Task<string> GetPasswordForNewEmployeeAsync(UserManager<Employee> userManager)
+        {
+            int number = await userManager.Users.CountAsync();
+            return $"employee-{number + 1}-temp";
+        }
+    }
+}
